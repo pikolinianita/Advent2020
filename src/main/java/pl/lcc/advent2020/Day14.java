6a22af14ef327addb34b3f9f8f262e069d09cac1
pikolinian <pikolinian@gazeta.pl>
2021-09-23 23:07:37 +0200

    day 14 part 1

---
+++ src/main/java/pl/lcc/advent2020/Day14.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lcc.advent2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
+
+
+class Mask implements UnaryOperator<BitSet> {
+    BitSet andMask; //1110 X->1
+    BitSet orMask;  //1000 X->0
+    
+    public Mask(String description){
+        andMask = new BitSet(description.length());
+        orMask = new BitSet(description.length());
+        for(int i =0; i<description.length(); i++){       
+            switch(description.charAt(i)){
+                case '1' -> {
+                    andMask.set(description.length() - i -1, true);
+                    orMask.set(description.length() - i -1, true);}
+                 case '0' -> {
+                    andMask.set(description.length() - i -1, false);
+                    orMask.set(description.length() - i -1, false);}
+                  case 'X' -> {
+                    andMask.set(description.length() - i -1, true);
+                    orMask.set(description.length() - i -1, false);}
+                  default -> throw new IllegalArgumentException("----"+description.charAt(i)+"----");
+            }
+        }            
+    }
+
+    @Override
+    public BitSet apply(BitSet t) {
+        t.and(andMask);
+        t.or(orMask);
+        return t;     
+    }    
+}
+
+public class Day14 {
+
+    Mask mask;
+    
+    HashMap<String,Long> registers;
+    
+    private final List<String> input;
+ 
+    public Day14(String path) throws FileNotFoundException {
+        try ( Scanner sc = new Scanner(new File(path))) {
+           input = sc.useDelimiter("\n")
+                   .tokens()
+                   .toList();
+        }
+    registers = new HashMap<>();    
+    }
+    
+    Day14(List<String> input){
+        this.input = input;
+        registers = new HashMap<>();   
+    }
+    
+    void calculate(){
+        Utils.printResult("Template",part1(),part2());
+    }
+    
+    long part1() {
+        input.forEach(this::process);
+        return getP1Answer();
+    }
+
+    int part2() {
+        return -1;
+    }
+
+    private long getP1Answer() {
+        System.out.println(registers);
+        return registers.values().stream().mapToLong(l -> l).sum();       
+                }
+
+    private void process(String line) {
+        if(line.charAt(1)=='a'){
+            mask = new Mask(line.substring(7));           
+        } else {
+            var numbers = line.split("(mem\\[)|(\\] = )");       
+            var afterMask = mask.apply(BitSet.valueOf(new long[]{Long.valueOf(numbers[2])})).toLongArray()[0];           
+            registers.put(numbers[1], afterMask);
+        }
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+       new Day14("day14.txt").calculate(); //5786255997050 was too low
+    }
+}
---
+++ src/test/java/pl/lcc/advent2020/Day14Test.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lcc.advent2020;
+
+import java.util.BitSet;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author piko
+ */
+public class Day14Test{
+    
+    List<String> commands = List.of("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X","mem[8] = 11","mem[7] = 101","mem[8] = 0");
+
+    String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+    
+    public Day14Test() {
+           }
+
+    @Test
+    void testSomeMask() {
+       var input = BitSetfromString("0101");
+       var sut = new Mask("0XX1");
+       var result = sut.apply(input).toString();
+       assertThat(result).isEqualTo("{0, 2}");
+    }
+    
+    @Test
+    void testTransparentMask() {
+       var input = BitSetfromString("0101");
+       var sut = new Mask("XXXX");
+       var result = sut.apply(input).toString();
+       assertThat(result).isEqualTo("{0, 2}");
+    }
+
+    @Test
+    void testZeroMask() {
+       var input = BitSetfromString("0101");
+       var sut = new Mask("0000");
+       var result = sut.apply(input).toString();
+       assertThat(result).isEqualTo("{}");
+    }
+    
+     @Test
+    void testFullMask() {
+       var input = BitSetfromString("0101");
+       var sut = new Mask("1111");
+       var result = sut.apply(input).toString();
+       assertThat(result).isEqualTo("{0, 1, 2, 3}");
+    }
+    
+    @Test
+    void testMaskToLong() {
+       var input = BitSetfromString("0101");
+       var sut = new Mask("1111");       
+       var result = sut.apply(input).toLongArray()[0];
+       var resStr = Long.toBinaryString(sut.apply(input).toLongArray()[0]);
+       assertThat(result).isEqualTo(15);
+       assertThat(resStr).isEqualTo("1111");
+    }
+    
+    @Test
+    void testCorrValues(){
+       var input = BitSetfromLong(11);
+       var sut = new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+       var result = sut.apply(input).toLongArray()[0];
+       assertThat(result).isEqualTo(73);
+    }
+    
+   @Test
+    void testOtherValues(){
+       var input = BitSetfromLong(101);
+       var sut = new Mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+       var result = sut.apply(input).toLongArray()[0];
+       assertThat(result).isEqualTo(101);
+    }   
+    
+    @Test
+    void p1Test(){
+       var result = new Day14(commands).part1();
+       assertThat(result).isEqualTo(165);
+    }
+    
+    
+    private BitSet BitSetfromLong(long l){
+             return BitSet.valueOf(new long[]{l});
+    }
+    
+    
+    private BitSet BitSetfromString(String s){
+        return BitSetfromLong(Long.valueOf(s,2));
+    }
+}
