a31e81f0c67c7093492c5b2102b11e0e8c3b2031
pikolinian <pikolinian@gazeta.pl>
2022-03-07 11:17:57 +0100

    day16 p2

--- src/main/java/pl/lcc/advent2020/Day16.java
+++ src/main/java/pl/lcc/advent2020/Day16.java
@@ -9,24 +9,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Day16 {
 
     Train train;
-    
-    Day16 (){
-    train = new Train();
-    }
-    
-    public Day16(String path) throws FileNotFoundException{
+
+    Day16() {
         train = new Train();
-         try ( Scanner sc = new Scanner(new File(path))) {
+    }
+
+    public Day16(String path) throws FileNotFoundException {
+        train = new Train();
+        try ( Scanner sc = new Scanner(new File(path))) {
             sc.useDelimiter("\n");
             String next = sc.next();
-            while(!next.isBlank()){
+            while (!next.isBlank()) {
                 train.addRange(Reader.parseCategories(next));
                 next = sc.next();
             }
@@ -34,72 +36,23 @@ public class Day16 {
             train.setMyTicket(Reader.parseTicket(sc.next()));
             sc.next();
             sc.next();
-            while(sc.hasNext()){
+            while (sc.hasNext()) {
                 train.addTicket(Reader.parseTicket(sc.next()));
             }
-         }
-    }
-    
-    
-    class MultiRange {
-
-        List<Range> ranges;
-
-        MultiRange() {
-            ranges = new ArrayList<>();
-        }
-
-        MultiRange addRange(Range r) {
-            for (int i = 0; i < ranges.size(); i++) {
-                if (r.canMerge(ranges.get(i))) {
-                    ranges.set(i, ranges.get(i).merge(r));
-                    return this;
-                }
-            }
-
-            ranges.add(r);
-            return this;
-        }
-
-        int size() {
-            return ranges.stream().mapToInt(Range::size).sum();
-        }
-
-    }
-
-    static record Range(String name, int min, int max) {
-
-        boolean isValid(int value) {
-            return value >= min && value <= max;
-        }
-
-        //assumes records should merge     
-        Range merge(Range other) {
-            return new Range(name,
-                    Math.min(this.min(), other.min()),
-                    Math.max(this.max(), other.max()));
-        }
-
-        boolean canMerge(Range other) {
-            return !((this.min() > other.max()) || (this.max() < other.min()));
-        }
-
-        int size() {
-            return max - min + 1;
         }
     }
 
+   
     record Ticket(List<Integer> numbers) {
+
     }
 
     class Train {
 
-        
         List<CategoryLine> ticketFields;
         List<Ticket> tickets;
         Ticket myTicket;
-        
-        
+
         Train() {
             ticketFields = new ArrayList<>();
             tickets = new ArrayList<>();
@@ -109,7 +62,7 @@ public class Day16 {
             this.myTicket = myTicket;
             return this;
         }
-        
+
         Train addRange(CategoryLine r) {
             ticketFields.add(r);
             return this;
@@ -120,66 +73,181 @@ public class Day16 {
             return this;
         }
 
-        List<Integer> getWrongValues() {
-            return null;
-        }
-        
-        int calculateP1(){
+        int calculateP1() {
             return tickets.stream()
-                    .flatMap(t->t.numbers().stream())
-                    .filter(n -> 0 == ticketFields.stream().filter(cat -> cat.inRange(n)).count())
-                    .mapToInt(i->i)
+                    .flatMap(t -> t.numbers().stream())
+                    .filter(n -> isNumberValid(n))
+                    .mapToInt(i -> i)
                     .sum();
         }
-        
+
+        private boolean isNumberValid(Integer n) {
+            return ticketFields.stream().noneMatch(cat -> cat.inRange(n));
+        }
+
+        boolean isTicketValid(Ticket t) {
+            return t.numbers.stream().noneMatch(this::isNumberValid);
+        }
+
+        List<Ticket> removeInvalidTickets() {
+            return tickets.stream().filter(this::isTicketValid).toList();
+        }
+
         @Override
         public String toString() {
             return "Train{" + "ticketFields=" + ticketFields + ", tickets=" + tickets + ", myTicket=" + myTicket + '}';
         }
-        
-        
+
     }
-    
-    record CategoryLine(String name, int min1, int max1, int min2, int max2){
-    
-    boolean inRange(int n){
-        return ((n >= min1) && (n <= max1)) || ((n >= min2) && (n <= max2));
+
+    record CategoryLine(String name, int min1, int max1, int min2, int max2) {
+
+        boolean inRange(int n) {
+            return ((n >= min1) && (n <= max1)) || ((n >= min2) && (n <= max2));
+        }
+
+        boolean inRange(List<Integer> list) {
+            return list.stream().allMatch(this::inRange);
+        }
+
     }
+
+    class P2Solver {
+
+        int size;
+
+        Map<Integer, List<Integer>> numbersInPosition;
+
+        Map<CategoryLine, List<Integer>> possibleSolutions;
+
+        Ticket myTicket;
+        
+          public P2Solver(int size, Day16.Train train) {
+           this.size = size;
+           numbersInPosition = new HashMap<>();
+           for (int i = 0; i < size; i++) {
+               numbersInPosition.put(i, new ArrayList<>());
+            }
+           myTicket = train.myTicket;
+            add(train.removeInvalidTickets());
+            add(train.myTicket);
+            findPossible(train.ticketFields);
+            
+        }
+
+       private P2Solver add(List<Ticket> tickets) {
+            tickets.forEach(this::addTicket);
+            return this;
+        }
+
+        private P2Solver add(Ticket ticket) {
+            addTicket(ticket);
+            return this;
+        }
+
+        private void addTicket(Ticket t) {
+            for (int i = 0; i < size; i++) {
+                numbersInPosition.compute(i, (n, list) -> {
+                    list.add(t.numbers.get(n));
+                    return list;
+                });
+            }
+        }
+
+       private P2Solver findPossible(List<CategoryLine> cats) {
+            possibleSolutions = new HashMap<>();
+            cats.forEach(cat -> possibleSolutions.put(cat, new ArrayList<>()));
+
+            cats.forEach(cat -> (numbersInPosition).entrySet().stream()
+                    .forEach(entry -> {
+                        if (cat.inRange(entry.getValue())) {
+                            possibleSolutions.compute(cat, (ct, list) -> {
+                                list.add(entry.getKey());
+                                return list;
+                            });
+                        }
+                    })
+            );
+
+            eliminateAlreadyFitted();
+            return this;
+        }
+
+        void eliminateAlreadyFitted() {
+
+            var workingList = possibleSolutions.entrySet().stream()
+                    .filter(entry -> entry.getValue().size() == 1)
+                    .map(entry -> entry.getKey())
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+            while (!workingList.isEmpty()) {
+
+                var element_code = workingList.remove(0);
+                var element = possibleSolutions.get(element_code).get(0);
+
+                possibleSolutions.entrySet().stream()
+                        .filter(entry -> entry.getValue().size() > 1)
+                        .forEach(entry -> {
+                            if (entry.getValue().remove(element)) {
+                                if (entry.getValue().size() == 1) {
+                                    workingList.add(entry.getKey());
+                                }
+                            }
+                        });
+            }
+
+        }
+
+        long getSolution() {
+            var targetList = List.of("departure location", "departure station", "departure platform", "departure track", "departure date", "departure time");
+            
+            return possibleSolutions.entrySet().stream()
+                    .filter(entry -> targetList.contains(entry.getKey().name))
+                    .map(entry -> entry.getValue().get(0))
+                    .map(pos -> myTicket.numbers.get(pos))
+                    .mapToLong(i -> i)
+                    .reduce((a, b) -> a * b)
+                    .getAsLong();
+        }
+
     }
 
     class Reader {
-        static CategoryLine parseCategories(String input){
+
+        static CategoryLine parseCategories(String input) {
             var name = input.substring(0, input.indexOf(':'));
             var numbers = Arrays.stream(input.replaceAll("[^0-9]+", " ").trim().split(" ")).mapToInt(Integer::parseInt).toArray();
-                          
-            return new CategoryLine(name,numbers[0],numbers[1],numbers[2],numbers[3]);
+
+            return new CategoryLine(name, numbers[0], numbers[1], numbers[2], numbers[3]);
         }
-        
-        static Ticket parseTicket(String input){
+
+        static Ticket parseTicket(String input) {
             var numbers = Arrays.stream(input
                     .replaceAll(",", " ")
                     .trim()
                     .split(" "))
                     .map(Integer::parseInt)
                     .collect(Collectors.toCollection(ArrayList<Integer>::new));
-                    
+
             return new Ticket(numbers);
         }
     }
-    
+
     public static void main(String[] args) throws FileNotFoundException {
-         new Day16 ("day16.txt").calculate();
+        new Day16("day16.txt").calculate();
     }
-    
+
     void calculate() {
-        Utils.printResult("Day 16", part1(), part2(null));
+        Utils.printResult("Day 16", part1(), part2());
     }
 
     int part1() {
-       return train.calculateP1();
+        return train.calculateP1();
     }
 
-    int part2(int[] input) {
-        return -1;
+    long part2() {
+        return new P2Solver(20,train)
+                .getSolution();
+
     }
 }
--- src/test/java/pl/lcc/advent2020/Day16Test.java
+++ src/test/java/pl/lcc/advent2020/Day16Test.java
@@ -5,8 +5,6 @@
  */
 package pl.lcc.advent2020;
 
-
-import java.io.FileNotFoundException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
@@ -16,26 +14,12 @@ import static org.assertj.core.api.Assertions.*;
  * @author piko
  */
 public class Day16Test {
-    
-    String sampleData = """
-                        class: 1-3 or 5-7
-                        row: 6-11 or 33-44
-                        seat: 13-40 or 45-50
-                        
-                        your ticket:
-                        7,1,14
-                        
-                        nearby tickets:
-                        7,3,47
-                        40,4,50
-                        55,2,20
-                        38,6,12""";
-    
-    public Day16Test() {       
+
+    public Day16Test() {
     }
-    
-    Day16.Train getTestTrain(){
-        return  new Day16() .new Train()
+
+    Day16.Train getTestTrain() {
+        return new Day16().new Train()
                 .addRange(Day16.Reader.parseCategories("class: 1-3 or 5-7"))
                 .addRange(Day16.Reader.parseCategories("row: 6-11 or 33-44"))
                 .addRange(Day16.Reader.parseCategories("seat: 13-40 or 45-50"))
@@ -45,66 +29,65 @@ public class Day16Test {
                 .addTicket(Day16.Reader.parseTicket("55,2,20"))
                 .addTicket(Day16.Reader.parseTicket("38,6,12"));
     }
-    
-    @Test
-    public void testSingle() {
-        var mr = new Day16().new MultiRange();
-        mr.addRange(new Day16.Range("", 4, 10));
-       assertThat(mr.size()).isEqualTo(7); 
+
+    Day16.Train getTestTrainP2() {
+        return new Day16().new Train()
+                .addRange(Day16.Reader.parseCategories("class: 0-1 or 4-19"))
+                .addRange(Day16.Reader.parseCategories("row: 0-5 or 8-19"))
+                .addRange(Day16.Reader.parseCategories("seat: 0-13 or 16-19"))
+                .setMyTicket(Day16.Reader.parseTicket("11,12,13"))
+                .addTicket(Day16.Reader.parseTicket("3,9,18"))
+                .addTicket(Day16.Reader.parseTicket("15,1,5"))
+                .addTicket(Day16.Reader.parseTicket("5,14,9"));
     }
-    
-    @Test
-    public void testTwoDis() {
-         var mr = new Day16().new MultiRange();
-         mr.addRange(new Day16.Range("", 4, 10)).addRange(new Day16.Range("", 12, 15));
-         assertThat(mr.size()).isEqualTo(11); 
-    }
-    
-    @Test
-    public void testTwoFirstL() {
-        var mr = new Day16().new MultiRange();
-         mr.addRange(new Day16.Range("", 4, 10)).addRange(new Day16.Range("", 5, 9));
-         assertThat(mr.size()).isEqualTo(7); 
-    }
-    
-    @Test
-    public void testTwoSecL() {
-         var mr = new Day16().new MultiRange();
-         mr.addRange(new Day16.Range("", 4, 10)).addRange(new Day16.Range("", 3, 11));
-         assertThat(mr.size()).isEqualTo(9); 
-    }
-    
-    @Test
-    public void testJoinFirstL() {
-        var mr = new Day16().new MultiRange();
-         mr.addRange(new Day16.Range("", 4, 10)).addRange(new Day16.Range("", 5, 11));
-         assertThat(mr.size()).isEqualTo(8); 
-    }
-    
-    @Test
-    public void testJoinSecL() {
-         var mr = new Day16().new MultiRange();
-         mr.addRange(new Day16.Range("", 4, 10)).addRange(new Day16.Range("", 3, 7));
-         assertThat(mr.size()).isEqualTo(8); 
-    }
-    
+
     @Test
     void parseRangeTest() {
         String input = "departure location: 47-164 or 179-960";
-        var suspectedResult = new Day16.CategoryLine("departure location",47,164,179,960);
+        var suspectedResult = new Day16.CategoryLine("departure location", 47, 164, 179, 960);
         assertThat(Day16.Reader.parseCategories(input)).isEqualTo(suspectedResult);
     }
-            
-     @Test
+
+    @Test
     void parseTicketTest() {
-    String input = "10,23,45";
-    var suspectedResult = new Day16.Ticket(List.of(10,23,45));
-    assertThat(Day16.Reader.parseTicket(input)).isEqualTo(suspectedResult);    
+        String input = "10,23,45";
+        var suspectedResult = new Day16.Ticket(List.of(10, 23, 45));
+        assertThat(Day16.Reader.parseTicket(input)).isEqualTo(suspectedResult);
+    }
+
+    @Test
+    void testTrain() {
+        var train = getTestTrain();
+        assertThat(train.calculateP1()).isEqualTo(71);
     }
     
     @Test
-    void testTrain(){
-       var train = getTestTrain();
-       assertThat(train.calculateP1()).isEqualTo(71);
+    void testTrainP2() {
+        
+        var sizeP1 = getTestTrain().removeInvalidTickets().size();
+        var sizeP2 = getTestTrainP2().removeInvalidTickets().size();
+        
+        assertThat(sizeP1).isEqualTo(1);
+        assertThat(sizeP2).isEqualTo(3);
     }
+    
+    @Test
+    void testMappedTicket(){
+        var train = getTestTrainP2();
+        var result = new Day16(). new P2Solver(3, train); 
+        
+        assertThat(result.numbersInPosition.get(0)).isEqualTo(List.of(3, 15, 5, 11));
+        assertThat(result.numbersInPosition.get(1)).isEqualTo(List.of(9, 1, 14, 12));
+        assertThat(result.numbersInPosition.get(2)).isEqualTo(List.of(18, 5, 9, 13));
+        assertThat(result.numbersInPosition.size()).isEqualTo(3);
+    }
+    
+    @Test
+    void testFindPossible(){
+        var train = getTestTrainP2();
+        var result = new Day16(). new P2Solver(3, train) ;
+
+        assertThat(result.possibleSolutions).hasSize(3).allSatisfy((i,list)->assertThat(list).hasSize(1));
+    }
+    
 }
