8a894d62ba798f1214d56a90bcebc2cc3970b88f
lcc <pikolinian@gmail.com>
2022-03-10 14:33:32 +0100

    day24

---
+++ src/main/java/pl/lcc/advent2020/Day24.java
@@ -0,0 +1,218 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.lcc.advent2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ *
+ * @author Nauczyciel
+ */
+public class Day24 {
+
+    List<String> directions;
+    
+    Day24(){};
+    
+    Day24(String path) throws FileNotFoundException{
+        try ( Scanner sc = new Scanner(new File(path))) {
+            sc.useDelimiter("\n");
+            directions = sc.tokens().toList();
+        }
+    }
+    
+    void calculate() {
+        Utils.printResult("Template", part1(), part2());
+    }
+
+    int part1() {
+       return new Board(directions).getAnswer();
+    }
+
+    int part2() {
+        return new Animator(new Board(directions), 100).result().getAnswer();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new Day24("day24.txt").calculate();
+    }
+
+    class Animator {
+
+        Set<Tile> sourceTiles;
+        int turnsRequired;
+        Set<Tile> resultTiles;
+        Map<Tile,Integer> whitesForChange;
+        
+        static List<Tile> neighbours = List.of(
+                new Tile (2,0),
+                new Tile (-2,0),
+                new Tile (1,1),
+                new Tile (-1,1),
+                new Tile (1,-1),
+                new Tile (-1,-1)
+        );
+        
+        public Animator(Board source, int n) {
+            sourceTiles = new HashSet(source.coloured);
+            turnsRequired = n;
+        }
+
+        Board result() {
+            resultTiles = new HashSet<>(sourceTiles);
+           // System.out.println(resultTiles);
+            for(int i =0; i<turnsRequired; i++){
+               // System.out.println(resultTiles);
+                //System.out.println("run tunn " +i + " out of " + turnsRequired);
+                resultTiles = doTurn();
+                //System.out.println(resultTiles);
+            }
+            
+            return new Board(resultTiles);
+        }
+
+        private Set<Tile> doTurn() {
+            whitesForChange = new HashMap<>();
+            List<Tile> blackToTurn = makeBlackToTurnListAndConstructWhitesMap();
+            List<Tile> whiteToTurn = filterWhiteMap();
+            
+            var result = new HashSet<>(resultTiles);
+            //System.out.println("black: " + blackToTurn);
+            //System.out.println("Whitr: " + whiteToTurn);
+            //System.out.println("map" + whitesForChange);
+            result.removeAll(blackToTurn);
+            result.addAll(whiteToTurn);
+            return result;
+        }
+
+        private List<Tile> makeBlackToTurnListAndConstructWhitesMap() {
+          return resultTiles.stream()
+                   .filter(this::checkNumbersAnsPopulateWhiteMap)
+                   .toList();
+        }
+
+        private List<Tile> filterWhiteMap() {
+           return whitesForChange.entrySet().stream()
+                    .filter(entry -> entry.getValue() == 2)
+                    .map (Entry::getKey)
+                    .toList();
+        }
+
+        private boolean checkNumbersAnsPopulateWhiteMap(Tile tile) {
+          var count = neighbours.stream()
+                    .map(tile::add)
+                    .filter( (t)-> 
+                    {if (resultTiles.contains(t)) return true; 
+                    else {addToWhitesMap(t);  return false;}})
+                    .count();
+          return !(count == 1 || count == 2);
+        }
+
+        private void addToWhitesMap(Tile t) {
+           // System.out.println("");
+         //   System.out.println(t);
+           // System.out.println(whitesForChange.get(t));
+            whitesForChange.merge(t, 1, (old, sth)-> ++old);
+            //System.out.println(whitesForChange.get(t));
+        }
+    }
+    
+    record Tile(int x, int y) {
+
+        Tile add (Tile t){
+            return new Tile (x + t.x, y + t.y);
+        }
+        
+    }
+ 
+    class Board {
+
+        Set<Tile> coloured;
+
+        Board() {
+            coloured = new HashSet<>();
+        }
+        
+        Board(Set<Tile> data){
+            coloured = data;
+        }
+     
+        Board(List<String> list) {
+            this();
+            coloured = list.stream()
+                    .map(this::parseString)
+                    .collect(HashSet::new,
+                            this::addToSet,
+                            (a, b) -> {
+                                throw new RuntimeException("Combiner should not be invoked");
+                            }
+                    );
+        }
+
+        void addToSet(Set<Tile> acc, Tile tile) {
+            if (acc.contains(tile)) {
+                acc.remove(tile);
+            } else {
+                acc.add(tile);
+            }
+
+        }
+
+        int getAnswer() {
+            return coloured.size();
+        }
+
+        Tile parseString(String s) {
+            int x = 0;
+            int y = 0;
+            try (var sc = new Scanner(s)) {
+                sc.useDelimiter("");
+                while (sc.hasNext()) {
+                    switch (sc.next()) {
+                        case "e" ->
+                            x += 2;
+                        case "w" ->
+                            x -= 2;
+                        case "n" -> {
+                            if ("e".equals(sc.next())) {
+                                x++;
+                                y++;
+                            } else {
+                                x--;
+                                y++;
+                            }
+                        }
+                        case "s" -> {
+                            if ("e".equals(sc.next())) {
+                                x++;
+                                y--;
+                            } else {
+                                x--;
+                                y--;
+                            }
+                        }
+                    }
+                }
+            }
+            return new Tile(x, y);
+        }
+        
+        Board animate(int n){
+            return new Animator(this,n).result();
+        }
+
+    }
+    
+    
+
+}
---
+++ src/test/java/pl/lcc/advent2020/Day24Test.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit3TestClass.java to edit this template
+ */
+package pl.lcc.advent2020;
+
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pl.lcc.advent2020.Day24.Tile;
+
+/**
+ *
+ * @author Nauczyciel
+ */
+
+public class Day24Test  {
+    
+     List<String> source = List.of(
+      "sesenwnenenewseeswwswswwnenewsewsw",
+"neeenesenwnwwswnenewnwwsewnenwseswesw",
+"seswneswswsenwwnwse",
+"nwnwneseeswswnenewneswwnewseswneseene",
+"swweswneswnenwsewnwneneseenw",
+"eesenwseswswnenwswnwnwsewwnwsene",
+"sewnenenenesenwsewnenwwwse",
+"wenwwweseeeweswwwnwwe",
+"wsweesenenewnwwnwsenewsenwwsesesenwne",
+"neeswseenwwswnwswswnw",
+"nenwswwsewswnenenewsenwsenwnesesenew",
+"enewnwewneswsewnwswenweswnenwsenwsw",
+"sweneswneswneneenwnewenewwneswswnese",
+"swwesenesewenwneswnwwneseswwne",
+"enesenwswwswneneswsenwnewswseenwsese",
+"wnwnesenesenenwwnenwsewesewsesesew",
+"nenewswnwewswnenesenwnesewesw",
+"eneswnwswnwsenenwnwnwwseeswneewsenese",
+"neswnwewnwnwseenwseesewsenwsweewe",
+"wseweeenwnesenwwwswnew");
+     
+    @Test
+    public void testCalculate() {
+    }
+
+    @Test
+    public void testPart1() {     
+       
+       var result = new Day24(). new Board(source).getAnswer();
+        assertThat(result).isEqualTo(10);
+        
+    }
+
+    @Test
+    public void testPart2() {
+    }
+    
+    
+    
+    @Test
+    void testOneTile(){
+         var result = new Day24() .new Board().parseString("sene");
+         var result2 = new Day24() .new Board().parseString("e");
+         var resultIdentity = new Day24() .new Board().parseString("nwwswee");
+         assertThat(result).isEqualTo(result2);
+         assertThat(resultIdentity).isEqualTo(new Day24.Tile(0,0));
+    }
+    
+    @Test
+    void testAnswerSix(){
+       var board =  new Day24() .new Board(List.of("e", "w", "se", "sw", "ne", "nw"));
+        assertThat(board.getAnswer()).isEqualTo(6);
+    }
+    
+    @Test
+    void testAnswerZero(){
+       var board =  new Day24() .new Board(List.of("e", "w", "se", "sw", "ne", "nw", "e", "w", "se", "sw", "ne", "nw"));
+        assertThat(board.getAnswer()).isEqualTo(0);
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+        "e, 2, 0",
+        "w , -2, 0",
+        "nw, -1, 1",
+        "ne, 1, 1",
+        "se, 1, -1",
+        "sw, -1, -1"         
+    })
+    void testRose(String s, int x, int y){
+       var result = new Day24() .new Board().parseString(s);
+       assertThat (result).isEqualTo(new Day24.Tile(x,y));
+    }
+    
+    @ParameterizedTest
+    @CsvSource({"1,15", "2,12", "3,25"
+            , "10,37", "20, 132"
+            , "100, 2208"
+    })
+    void testAnimation(int n, int result){
+        var day = new Day24();
+        var boardAfterAnimation = day.new Animator(day. new Board(source),n).result();
+        assertThat(boardAfterAnimation.getAnswer()).isEqualTo(result);
+    }
+    
+    @Test
+    void testOneTurn(){
+        var day = new Day24();
+        var src = Set.of(new Tile(0,0),new Tile (2,0));
+        var boardAfterAnimation = day.new Animator(day. new Board(src),3).result();
+        System.out.println(boardAfterAnimation.getAnswer());
+    }
+    
+}
